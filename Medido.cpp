decd244e7d772de096c51eb504e9283f10e4f62f
Shi4 <4andrewtakeshi>
2018-04-16 14:25:51 -0300

    Inciei

---
+++ Medido.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void Medidor() {
+	cout << "Digite o nível " << endl;
+	float num;
+	cin >> num;
+	if (num > 0) {
+		cour << "Safadão " << endl;
+	} 
+	cout << "Badass" << endl;
+}
+
+int main() {
+	
+	Medidor();
+	
+	return 0;
+}
