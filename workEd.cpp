14a479f12fb4c8fbb58521802e3b1e1e0ccb3834
Shi4 <4andrewtanakashi>
2018-05-06 04:57:38 -0300

    O primeiro de muitos

---
+++ workEd.cpp
@@ -0,0 +1,62 @@
+/* 
+  --Trabalho de Hashing Extensível :
+    Estrutura de Dados.
+  --Método de ++++++++++++++ 
+     (aqui vai o nome do seu trabalho)
+  --Copyright 2018 by Lucas, Andrew e Vinicius.  
+  --Arquivo contém as implementações das classes;
+*/
+
+#include <iostream>
+#include "workEd.h"
+
+/*int id;
+	char nome[50];
+	char dominio[10];
+	char biografia[200]*/
+
+void Menu ();
+
+Noh::Noh () {
+	mProximo = NULL;
+	mConteudo.id = -1;
+	mConteudo.nome[50] = ' ';
+	mConteudo.dominio[10] = ' ';
+	mConteudo.biografia[200] = ' ';
+}
+
+Noh::~Noh () {
+	mProximo = NULL;
+	mConteudo.id = -1;
+	mConteudo.nome[50] = ' ';
+	mConteudo.dominio[10] = ' ';
+	mConteudo.biografia[200] = ' ';
+}
+
+TabelaH::TabelaH (unsigned int capsup) : mCapacidade (capsup) {
+	mElementos = new Noh*[capsup];
+	for (int i = 0; i < capsup; ++i) {
+		mElementos[i] = NULL;
+	}
+}
+
+TabelaH::~TabelaH () {
+	for (int i = 0; i < mCapacidade; ++i) {
+		Noh* aquelequemorre = mElementos[i];
+		while (aquelequemorre != NULL) {
+			Noh* aux = aquelequemorre;
+			aquelequemorre = aquelequemorre->mProximo;
+			delete aux;
+		}
+	}
+	delete[] mElementos;
+}
+
+void Menu() {
+	cout <<"***********************************************" << endl;
+	cout <<"   1 - Inserir novo ---- no arquivo "<< endl;
+	cout <<"   2 -  Remover ---- do arquivo"<< endl;
+	cout <<"   3 - Consultar ----- no arquivo "<< endl;
+	cout <<"   4 - Imprimir todos registros "<< endl;
+	cout <<"*************************************************" << endl;
+}
---
+++ workEd.h
@@ -0,0 +1,50 @@
+/* 
+  --Trabalho de Hashing Extensível :
+    Estrutura de Dados.
+  --Método de ++++++++++++++ 
+     (aqui vai o nome do seu trabalho)
+  --Copyright 2018 by Lucas, Andrew e Vinicius.  
+  --Arquivo contém as classes;
+*/
+
+#ifndef WORKED_H
+#define WORKED_H
+
+#include <iostream>
+#define TAM_BLOCO 16
+
+using namespace std;
+
+struct Deuses {
+	int id;
+	char nome[50];
+	char dominio[10];
+	char biografia[200];
+};
+
+typedef Deuses Dado;
+
+class Noh {
+	friend class TabelaH;
+	public:
+		Noh ();
+		~Noh ();
+	protected:
+		Noh* mProximo;
+		Dado mConteudo;
+};
+
+class TabelaH {
+	public:
+		TabelaH (unsigned int capsup = 100);
+		~TabelaH ();
+		void Insere (Dado* all);
+		void Remove (int ident);
+		void Percorre ();
+	protected :
+		Noh** mElementos;
+		unsigned int mCapacidade;
+
+};
+
+#endif
